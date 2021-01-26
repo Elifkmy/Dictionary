014a36bc674c311af6caaac683b2d88f3bf3bb41
KAMAY <KAMAY@DESKTOP-FKU642V>
2021-01-26 09:55:21 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    
+    class MyDictionary<T,V>
+    {
+        T[] keys;
+        V[] values;
+        public MyDictionary()
+        {
+            keys = new T[0];
+            values = new V[0];
+        }
+
+        public void Add1(T key,V value)
+        {
+            T[] tempArray_Key = keys;
+            V[] tempArray_Value = values;
+
+            keys = new T[keys.Length + 1];
+            for (int i = 0; i < tempArray_Key.Length; i++)
+            {
+                keys[i] = tempArray_Key[i];
+            }
+
+            keys[keys.Length - 1] = key;
+
+            values = new V[values.Length + 1];
+            for (int i = 0; i <tempArray_Value.Length; i++)
+            {
+                values[i] = tempArray_Value[i];
+            }
+
+            values[values.Length - 1] = value;
+        }
+       
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyDictionary<string, long> persons = new MyDictionary<string, long>();
+            persons.Add1("Kerem Yılmaz",12345678901);
+            persons.Add1("Hilal Tosun", 78796324504);
+
+            Console.WriteLine("Kişi kaydı oluşturulmuştur.");
+        }
+    }
+}
